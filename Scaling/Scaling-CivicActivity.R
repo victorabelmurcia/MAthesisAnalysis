09eac6f43f834bcfb52920bcbc7cf4a66abb5b8c
sztal <sztal111@gmail.com>
2014-11-29 19:27:50 +0100

    Update Scaling/Scaling-CivicActivity.R (still only beginning)

--- Scaling/Scaling-CivicActivity.R
+++ Scaling/Scaling-CivicActivity.R
@@ -1,7 +1,11 @@
 ### This script describes and replicates a scaling procedure
 ### that has been used to scale data on social/civic activity
 ### variables: from socact1 to socact14
+### I employ mokken scaling (and use "mokken" package)
+library(mokken)
 
 D = read.csv("Dane/DataInd218.csv")
 # Restrict the dataset only to civic activity (and id)
 D = D[, c(1, which(names(D)=="socact1"):which(names(D)=="socact14"))]
+
+Diff = apply(D[,2:15], 2, mean, na.rm=T) # Difficulty levels
