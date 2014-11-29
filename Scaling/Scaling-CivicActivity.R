f1ec50be2904edf177ab6c8dbb431f698210dd00
sztal <sztal111@gmail.com>
2014-11-29 19:09:50 +0100

    Add Scaling/Scaling-CivicActivity.R (not yet finished)

--- Scaling/Scaling-CivicActivity.R
+++ Scaling/Scaling-CivicActivity.R
@@ -1,3 +1,7 @@
 ### This script describes and replicates a scaling procedure
 ### that has been used to scale data on social/civic activity
 ### variables: from socact1 to socact14
+
+D = read.csv("Dane/DataInd218.csv")
+# Restrict the dataset only to civic activity (and id)
+D = D[, c(1, which(names(D)=="socact1"):which(names(D)=="socact14"))]
