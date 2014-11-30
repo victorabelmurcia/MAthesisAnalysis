fc5fe90b7b81fffbb438f6c76a02f731a19ebaa7
sztal <sztal111@gmail.com>
2014-11-30 19:56:38 +0100

    Update "ImputeGenMICE.R" (up to cultural preferenses) but the plotting is not finished yet!

--- MissImpute/ImputeGenMICE.R
+++ MissImpute/ImputeGenMICE.R
@@ -96,3 +96,130 @@ legend("topright", c("SE", "RMSE"), lty=c(1,2), cex=.7)
 par(mfrow=c(1,1))
 ##################################################
 
+
+# MICE imputation - social / civic activity
+# Method: logistic regression (since the data is binary)
+Act = D[,which(names(D)=="socact1"):which(names(D)=="socact14")]
+# for technical reasons the data has to be transformed into factors
+# in order to use logistic regression as the imputation method
+for(i in 1:dim(Act)[2]) Act[,i] = as.factor(Act[,i])
+ActNAs = apply(Act, 2, numNA) # number of NAs in Act; there are very few of them
+ActImp = mice(Act, m=20, seed=2515, method="logreg")
+ActImpL = actualImp(ActImp$imp)
+ActImpErr = ImpOut(ActImpL, Act)[[1]] # Uncertainity of imputed values
+ActImpVal = ImpOut(ActImpL, Act)[[2]] # Dominant imputed values
+Act = mapImpToData(ActImpVal, Act)  # Map imputed values back to the dataset
+# Transfrom Act back to numeric
+for(i in 1:dim(Act)[2]) Act[,i] = as.numeric(as.character(Act[,i]))
+ActRMSE = meanImpRMSE(ActImp, Act, factor_imp=TRUE)
+ActSE = apply(Act, 2, se, na.rm=TRUE)
+#########################################
+# Plot RMSE against SE and entropy bars
+#########################################
+# Setting up a plot
+par(mfrow=c(2,1))
+barplot(ActImpErr[,3], ylim=c(0,1), col="gray15", las=3, ylab="Entropia względna")
+# Here the RMSE / SE plot begins
+plot(ActSE, type="l", ylim=c(0,.05), xlim=c(0,length(ActSE)), ylab="SE / RMSE", xlab="", xaxt="n", lwd=2, lty=1)
+axis(1, at=1:length(ActSE), labels=names(ActSE), las=3)
+abline(v=15.5, lty=2)
+lines(ActRMSE, lwd=2, lty=2)
+text(7,.045, "Aktywność społeczna / obywatelska", cex=.7)
+legend("topright", c("SE", "RMSE"), lty=c(1,2), cex=.7)
+# Here the plot ends
+par(mfrow=c(1,1))
+##################################################
+
+# MICE imputation for press, tv, cinema and books
+# Method: predictive mean matching (pmm)
+vars = c(20:37,  39:49, 51:61, 63:79, 81:94)
+Cult = D[,vars]
+CultNAs = apply(Cult, 2, numNA) # number of NAs in Cult;
+# There is much more of them than in the previous cases
+CultImp = mice(Cult, m=20, seed=9999, method="pmm")
+CultImpL = actualImp(CultImp$imp)
+CultImpErr = ImpOut(CultImpL, Cult)[[1]] # Uncertainity of imputed values
+CultImpVal = ImpOut(CultImpL, Cult)[[2]] # Dominant imputed values
+Cult = mapImpToData(CultImpVal, Cult)  # Map imputed values back to the dataset
+CultRMSE = meanImpRMSE(CultImp, Cult)
+CultSE = apply(Cult, 2, se, na.rm=TRUE)
+#########################################
+# Plot RMSE against SE and entropy bars
+# - tv preferences
+#########################################
+# Setting up a plot
+par(mfrow=c(2,1))
+barplot(CultImpErr[1:18,3], ylim=c(0,1), col="gray15", las=3, ylab="Entropia względna")
+# Here the RMSE / SE plot begins
+plot(CultSE[1:18], type="l", ylim=c(0,.2), xlim=c(0,length(CultSE[1:18])), ylab="SE / RMSE", xlab="", xaxt="n", lwd=2, lty=1)
+axis(1, at=1:length(CultSE[1:18]), labels=names(CultSE[1:18]), las=3)
+lines(CultRMSE[1:18], lwd=2, lty=2)
+text(8,.19, "Preferencje - TV", cex=.7)
+legend("topright", c("SE", "RMSE"), lty=c(1,2), cex=.7)
+# Here the plot ends
+par(mfrow=c(1,1))
+##################################################
+# Plot RMSE against SE and entropy bars
+# - Movie / cinema preferences
+#########################################
+# Setting up a plot
+par(mfrow=c(2,1))
+barplot(CultImpErr[19:29,3], ylim=c(0,1), col="gray15", las=3, ylab="Entropia względna")
+# Here the RMSE / SE plot begins
+plot(CultSE[19:29], type="l", ylim=c(0,.3), xlim=c(0,length(CultSE[19:29])), ylab="SE / RMSE", xlab="", xaxt="n", lwd=2, lty=1)
+axis(1, at=1:length(CultSE[19:29]), labels=names(CultSE[19:29]), las=3)
+lines(CultRMSE[19:29], lwd=2, lty=2)
+text(5,.27, "Preferencje - filmy / kino", cex=.7)
+legend("topleft", c("SE", "RMSE"), lty=c(1,2), cex=.7)
+# Here the plot ends
+par(mfrow=c(1,1))
+##################################################
+# Plot RMSE against SE and entropy bars
+# - Literary / book preferences
+#########################################
+# Setting up a plot
+par(mfrow=c(2,1))
+barplot(CultImpErr[30:40,3], ylim=c(0,1), col="gray15", las=3, ylab="Entropia względna")
+# Here the RMSE / SE plot begins
+plot(CultSE[30:40], type="l", ylim=c(0,.3), xlim=c(0,length(CultSE[30:40])), ylab="SE / RMSE", xlab="", xaxt="n", lwd=2, lty=1)
+axis(1, at=1:length(CultSE[30:40]), labels=names(CultSE[30:40]), las=3)
+lines(CultRMSE[30:40], lwd=2, lty=2)
+text(6,.27, "Preferencje - książki / literatura", cex=.7)
+legend("topleft", c("SE", "RMSE"), lty=c(1,2), cex=.7)
+# Here the plot ends
+par(mfrow=c(1,1))
+##################################################
+# Plot RMSE against SE and entropy bars
+# - Press preferences
+#########################################
+# Setting up a plot
+par(mfrow=c(2,1))
+barplot(CultImpErr[31:57,3], ylim=c(0,1), col="gray15", las=3, ylab="Entropia względna")
+# Here the RMSE / SE plot begins
+plot(CultSE[41:57], type="l", ylim=c(0,.3), xlim=c(0,length(CultSE[41:57])), ylab="SE / RMSE", xlab="", xaxt="n", lwd=2, lty=1)
+axis(1, at=1:length(CultSE[41:57]), labels=names(CultSE[41:57]), las=3)
+lines(CultRMSE[41:57], lwd=2, lty=2)
+text(7,.27, "Preferencje - prasa", cex=.7)
+legend("topleft", c("SE", "RMSE"), lty=c(1,2), cex=.7)
+# Here the plot ends
+par(mfrow=c(1,1))
+##################################################
+# Plot RMSE against SE and entropy bars
+# - Press preferences
+#########################################
+# Setting up a plot
+par(mfrow=c(2,1))
+barplot(CultImpErr[31:57,3], ylim=c(0,1), col="gray15", las=3, ylab="Entropia względna")
+# Here the RMSE / SE plot begins
+plot(CultSE[41:57], type="l", ylim=c(0,.3), xlim=c(0,length(CultSE[41:57])), ylab="SE / RMSE", xlab="", xaxt="n", lwd=2, lty=1)
+axis(1, at=1:length(CultSE[41:57]), labels=names(CultSE[41:57]), las=3)
+lines(CultRMSE[41:57], lwd=2, lty=2)
+text(7,.27, "Preferencje - prasa", cex=.7)
+legend("topleft", c("SE", "RMSE"), lty=c(1,2), cex=.7)
+# Here the plot ends
+par(mfrow=c(1,1))
+##################################################
+
+
+# Save the workspace (in order not to repeat the computationally heavy analysis)
+save.image("MissImpute/MICEimput.RData")
