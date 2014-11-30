0bf4ddee2e86354a0ead7fed76966a8b394a1d49
sztal <sztal111@gmail.com>
2014-11-30 18:36:03 +0100

    Update "ImputeGenMICE.R" up to social capital

--- MissImpute/ImputeGenMICE.R
+++ MissImpute/ImputeGenMICE.R
@@ -9,6 +9,7 @@
 
 library(mice)
 library(dplyr)
+library(lattice)
 source("MissImpute/ImputeHelper.R")
 source("GenCompFuncs/ComputingMisc.R")
 
@@ -35,15 +36,63 @@ D[!is.na(D$tvtime) & D$tvtime==0,(which(names(D)=="tv.a1")-1):which(names(D)=="t
 
 D = arrange(D, id) # sort the dataset by id.
 
-# In general, MICE algorithm will be used to generate 10 parallel datasets with imputed values. This will give higher chances of getting an optimal solution and moreover will allow estimating uncertainity of every of replacement.
+# In general, MICE algorithm will be used to generate 20 parallel datasets with imputed values. This will give higher chances of getting an optimal solution and moreover will allow estimating uncertainity of every of replacement.
 
 # MICE imputation - place attachment scales
 # method of imputation - predictive mean matching (pmm)
 Att = D[,which(names(D)=="attgen1"):which(names(D)=="attmult18")]
 AttNAs = apply(Att, 2, numNA) # number of NAs in Att; there are very few of them
-AttImp = mice(Att, m=10, seed=101)
-AttImp = actualImp(AttImp$imp)
-AttImpErr = ImpOut(AttImp, Att)[[1]] # Uncertainity of imputed values
-AttImpVal = ImpOut(AttImp, Att)[[2]] # Dominant imputed values
+AttImp = mice(Att, m=20, seed=101, method="pmm") # seed sets up a pseudo-random numbers generator
+AttImpL = actualImp(AttImp$imp)
+AttImpErr = ImpOut(AttImpL, Att)[[1]] # Uncertainity of imputed values
+AttImpVal = ImpOut(AttImpL, Att)[[2]] # Dominant imputed values
 Att = mapImpToData(AttImpVal, Att)  # Map imputed values back to the dataset
+AttRMSE = meanImpRMSE(AttImp, Att)
+AttSE = apply(Att, 2, se, na.rm=TRUE)
+#########################################
+# Plot RMSE against SE and entropy bars
+#########################################
+# Setting up a plot
+par(mfrow=c(2,1))
+barplot(AttImpErr[,3], ylim=c(0,1), col="gray15", las=3, ylab="Entropia względna")
+# Here the RMSE / SE plot begins
+plot(AttSE, type="l", ylim=c(0,.15), xlim=c(0,30), ylab="SE / RMSE", xlab="", xaxt="n", lwd=2, lty=1)
+axis(1, at=1:length(AttSE), labels=names(AttSE), las=3)
+abline(v=9.5, lty=2)
+lines(AttRMSE, lwd=2, lty=2)
+text(4,.14, "Przywiązanie - jedenwymiar", cex=.7)
+text(20,.14, "Przywiązanie - 3 wymiary", cex=.7)
+legend("topright", c("SE", "RMSE"), lty=c(1,2), cex=.7)
+# Here the plot ends
+par(mfrow=c(1,1))
+##################################################
+
+# MICE imputation - social capital scales (knowing and getting help - Resource Generator)
+# Method: predictive mean meathing (pmm)
+SC = D[,which(names(D)=="scknow1"):which(names(D)=="schelp15")]
+SCNAs = apply(SC, 2, numNA) # number of NAs in SC; there are very few of them
+SCImp = mice(SC, m=20, seed=1717, method="pmm")
+SCImpL = actualImp(SCImp$imp)
+SCImpErr = ImpOut(SCImpL, SC)[[1]] # Uncertainity of imputed values
+SCImpVal = ImpOut(SCImpL, SC)[[2]] # Dominant imputed values
+SC = mapImpToData(SCImpVal, SC)  # Map imputed values back to the dataset
+SCRMSE = meanImpRMSE(SCImp, SC)
+SCSE = apply(SC, 2, se, na.rm=TRUE)
+#########################################
+# Plot RMSE against SE and entropy bars
+#########################################
+# Setting up a plot
+par(mfrow=c(2,1))
+barplot(SCImpErr[,3], ylim=c(0,1), col="gray15", las=3, ylab="Entropia względna")
+# Here the RMSE / SE plot begins
+plot(SCSE, type="l", ylim=c(0,.15), xlim=c(0,length(SCSE)), ylab="SE / RMSE", xlab="", xaxt="n", lwd=2, lty=1)
+axis(1, at=1:length(SCSE), labels=names(SCSE), las=3)
+abline(v=15.5, lty=2)
+lines(SCRMSE, lwd=2, lty=2)
+text(8,.14, "Kapitał - znajomość", cex=.7)
+text(22,.14, "Kapitał - pomoc", cex=.7)
+legend("topright", c("SE", "RMSE"), lty=c(1,2), cex=.7)
+# Here the plot ends
+par(mfrow=c(1,1))
+##################################################
 
